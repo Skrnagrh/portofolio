a3a1fb54b57c363b10bf44dfa4a6df4e5a9f62be
skrnagrh <suk12on512@gmail.com>
2023-12-14 10:06:56 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './experience.html'],
   darkMode: 'class',
   theme: {
     container:{
