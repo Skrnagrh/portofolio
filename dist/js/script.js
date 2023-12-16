8ff32da2bba97738cea449e29d9306423510a21e
skrnagrh <suk12on512@gmail.com>
2023-12-16 09:11:56 +0700

    portofolio vektor

--- dist/js/script.js
+++ dist/js/script.js
@@ -58,3 +58,32 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   // untuk tahun di footer
 const currentYear = new Date().getFullYear();
 document.getElementById("current-year").textContent = currentYear;
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade:'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicsBullets:true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+      0:{
+          slidesPerView: 1,
+      },
+      520:{
+          slidesPerView: 2,
+      },
+      950:{
+          slidesPerView: 3,
+      },
+    },
+  });
