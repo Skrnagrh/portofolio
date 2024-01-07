2824ae974130cd4c3c8d0cd1ae97d9194f4e29d4
skrnagrh <suk12on512@gmail.com>
2024-01-07 15:02:28 +0700

    I love youuuuuuuuu

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,13 +17,33 @@ window.onscroll = function(){
 
 
 // Hamburger
+// const hamburger = document.querySelector('#hamburger');
+// const navMenu = document.querySelector('#nav-menu');
+
+// hamburger.addEventListener('click', function(){
+//     hamburger.classList.toggle('hamburger-active');
+//     navMenu.classList.toggle('hidden');
+// });
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
+
+// Function to close the navigation menu
+function closeNavMenu() {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+}
 
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// Event listener for the scroll event on the window
+window.addEventListener('scroll', function() {
+    // Close the navigation menu when the user scrolls
+    closeNavMenu();
+});
+
 
 // Klik di luar Humberger
 window.addEventListener('click', function(e){
